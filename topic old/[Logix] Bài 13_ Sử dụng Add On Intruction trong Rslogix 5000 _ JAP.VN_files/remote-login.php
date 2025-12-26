6c178c69cc530255f9ede92cfe36c7d52fecf689
DalatmilkSHOP <228861937+dalatmilkshop@users.noreply.github.com>
2025-12-26 16:34:55 +0700

    1

---
+++ topic old/[Logix] Bài 13_ Sử dụng Add On Intruction trong Rslogix 5000 _ JAP.VN_files/remote-login.php
@@ -0,0 +1,31 @@
+var _____WB$wombat$assign$function_____=function(name){return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name))||self[name];};if(!self.__WB_pmw){self.__WB_pmw=function(obj){this.__WB_source=obj;return this;}}{
+let window = _____WB$wombat$assign$function_____("window");
+let self = _____WB$wombat$assign$function_____("self");
+let document = _____WB$wombat$assign$function_____("document");
+let location = _____WB$wombat$assign$function_____("location");
+let top = _____WB$wombat$assign$function_____("top");
+let parent = _____WB$wombat$assign$function_____("parent");
+let frames = _____WB$wombat$assign$function_____("frames");
+let opens = _____WB$wombat$assign$function_____("opens");
+
+}
+/*
+     FILE ARCHIVED ON 04:08:40 Mar 09, 2014 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 09:13:51 Dec 26, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 269.67
+  exclusion.robots: 0.017
+  exclusion.robots.policy: 0.007
+  esindex: 0.009
+  cdx.remote: 40.954
+  LoadShardBlock: 171.492 (3)
+  PetaboxLoader3.datanode: 108.502 (4)
+  PetaboxLoader3.resolve: 278.677 (2)
+  load_resource: 289.82
+*/
---
+++ topic old/[Logix] Bài 14_ Trao đổi dữ liệu giữa các Controller (Controllogix_Compactlogix) _ JAP.VN_files/remote-login.php
@@ -0,0 +1,31 @@
+var _____WB$wombat$assign$function_____=function(name){return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name))||self[name];};if(!self.__WB_pmw){self.__WB_pmw=function(obj){this.__WB_source=obj;return this;}}{
+let window = _____WB$wombat$assign$function_____("window");
+let self = _____WB$wombat$assign$function_____("self");
+let document = _____WB$wombat$assign$function_____("document");
+let location = _____WB$wombat$assign$function_____("location");
+let top = _____WB$wombat$assign$function_____("top");
+let parent = _____WB$wombat$assign$function_____("parent");
+let frames = _____WB$wombat$assign$function_____("frames");
+let opens = _____WB$wombat$assign$function_____("opens");
+
+}
+/*
+     FILE ARCHIVED ON 08:50:43 Feb 07, 2014 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 09:13:53 Dec 26, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.519
+  exclusion.robots: 0.015
+  exclusion.robots.policy: 0.007
+  esindex: 0.009
+  cdx.remote: 6.116
+  LoadShardBlock: 230.766 (3)
+  PetaboxLoader3.resolve: 408.039 (3)
+  PetaboxLoader3.datanode: 137.339 (4)
+  load_resource: 362.558
+*/
